7be217ec127d00a4a311c9e10b89ce5320a126eb
jkmoore <jkm9@pdx.edu>
2023-10-03 20:33:42 +0900

    feat: add github and mail links, styling

--- src/components/Subheader.tsx
+++ src/components/Subheader.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  a:hover {
+    color: #00D816;
+  }
+`;
 
 export default function Header({ subheader }) {
   if (!subheader) {
     return null;
   }
+  const githubFullLink = "https://" + subheader.github;
+  const mailToLink = "mailto:" + subheader.email;
   return (
-    <div>
-      <p>{subheader.email}</p>
-      <p>{subheader.github}</p>
-    </div>
+    <StyledDiv>
+      <a href={mailToLink}><p>{subheader.email}</p></a>
+      <a href={githubFullLink}><p>{subheader.github}</p></a>
+    </StyledDiv>
   );
 }
