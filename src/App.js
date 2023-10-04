f37f2b148375a7029362d471db1c7912d60996de
jkmoore <jkm9@pdx.edu>
2023-10-04 19:17:41 +0900

    feat: add style breakpoints for div width

--- src/App.js
+++ src/App.js
@@ -3,19 +3,40 @@ import Section from './components/Section.tsx';
 import Header from './components/Header.tsx';
 import Subheader from './components/Subheader.tsx';
 import pageText from './pageText.json';
-import styled from "styled-components";
+import { styled, ThemeProvider } from 'styled-components';
+import { createStyledBreakpointsTheme } from 'styled-breakpoints';
+
+const theme = createStyledBreakpointsTheme();
 
 const StyledDiv = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
+  ${({ theme }) => theme.breakpoints.down('sm')} {
+    div {
+      max-width: 16rem;
+      font-size: 0.75rem;
+    }
+  }
+  ${({ theme }) => theme.breakpoints.between('sm', 'md')} {
+    div {
+      max-width: 28rem;
+    }
+  }
+  ${({ theme }) => theme.breakpoints.between('md', 'xl')} {
+    div {
+      max-width: 38rem;
+    }
+  }
+  ${({ theme }) => theme.breakpoints.up('xl')} {
+    div {
+      max-width: 58rem;
+    }
+  }
   div {
-    width: 48rem;
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: left;
+    width: 100%;
   }
   * {
     margin-block-start: 0.67em;
@@ -30,12 +51,14 @@ const StyledDiv = styled.div`
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <StyledDiv>
       <Header header={pageText.header} />
       <Subheader subheader={pageText.subheader} />
       <Section section={pageText.experience} />
       <Section section={pageText.projects} />
     </StyledDiv>
+    </ThemeProvider>
   );
 }
 
