858585545781765cce640360044de292f79ab4ea
jkmoore <jkm9@pdx.edu>
2023-10-05 22:17:46 +0900

    feat: add pageText

---
+++ src/pageText.js
@@ -0,0 +1,45 @@
+export const pageText = `{
+  "header": {
+    "greeting": "Hi, I'm Kai Moore.",
+    "desc": "Software Engineer at Rakuten"
+  },
+  "subheader": {
+    "email": "jkmoore817@gmail.com",
+    "github": "github.com/jkmoore"
+  },
+  "experience": {
+    "header": "Experience",
+    "content": [
+      {
+        "title": "Software Engineer at Rakuten",
+        "link": "https://global.rakuten.com/corp/",
+        "time": "Apr 2022 - Present",
+        "desc": "Improved Angular user interface and updated React components in no-code platform\\nRefactoring B2B React web application for performance improvements\\nImproving UI with React, styled-components"
+      },
+      {
+        "title": "Bilingual Programming & Robotics Instructor at ROBBO",
+        "link": "https://www.robbo.world",
+        "time": "Aug 2021 - Dec 2021",
+        "desc": "Led lectures and activities teaching programming and engineering to children ages 8-13"
+      },
+      {
+        "title": "Grader at PSU Dept. of Computer Science",
+        "link": "https://www.pdx.edu/computer-science/",
+        "time": "Oct 2018 - Mar 2021",
+        "desc": "Scored and gave feedback on C++/Java programs for students in university Data Structures and OOP courses"
+      }
+    ]
+  },
+  "projects": {
+    "header": "Projects",
+    "content": [
+      {
+        "title": "jkmoore.github.io",
+        "link": "http://jkmoore.github.io",
+        "time": "Sep 2023",
+        "desc": "My online portfolio. React, styled-components."
+      }
+    ]
+  }
+}
+`
