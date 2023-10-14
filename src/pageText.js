e1ddc358fb3dd9f36c9aa297b5707423ced46d5f
jkmoore <jkm9@pdx.edu>
2023-10-14 16:29:06 +0900

    feat: update page text

--- src/pageText.js
+++ src/pageText.js
@@ -14,19 +14,19 @@ export const pageText = `{
         "title": "Software Engineer at Rakuten",
         "link": "https://global.rakuten.com/corp/",
         "time": "Apr 2022 - Present",
-        "desc": "Improved Angular user interface and updated React components in no-code platform\\nRefactoring B2B React web application for performance improvements\\nImproving UI with React, styled-components"
+        "desc": "Developing and improving B2B web apps for Rakuten Mobile, including a low-code app builder and a phone plan customer site.\\nReact, CSS, Angular, NestJS"
       },
       {
         "title": "Bilingual Programming & Robotics Instructor at ROBBO",
         "link": "https://www.robbo.world",
         "time": "Aug 2021 - Dec 2021",
-        "desc": "Led lectures and activities teaching programming and engineering to children ages 8-13"
+        "desc": "Led lectures and activities teaching programming and engineering to children ages 8-13."
       },
       {
         "title": "Grader at PSU Dept. of Computer Science",
         "link": "https://www.pdx.edu/computer-science/",
         "time": "Oct 2018 - Mar 2021",
-        "desc": "Scored and gave feedback on C++/Java programs for students in university Data Structures and OOP courses"
+        "desc": "Scored and gave feedback on C++/Java programs for students in university Data Structures and OOP courses."
       }
     ]
   },
@@ -34,10 +34,16 @@ export const pageText = `{
     "header": "Projects",
     "content": [
       {
+        "title": "What's for Dinner?",
+        "time": "Ongoing",
+        "link": "https://github.com/jkmoore/whats-for-dinner",
+        "desc": "A tool for the home chef to manage their personal inventory, recipes, and more.\\nReact, Firebase"
+      },
+      {
         "title": "jkmoore.github.io",
         "link": "https://github.com/jkmoore/jkmoore.github.io",
         "time": "Oct 2023",
-        "desc": "My online portfolio. React, styled-components."
+        "desc": "My online portfolio.\\nReact, styled-components"
       }
     ]
   }
