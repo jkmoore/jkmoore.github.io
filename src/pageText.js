f1d3a56b74cda6776894d44ba2e1b048890a18a7
jkmoore <jkm9@pdx.edu>
2023-11-18 21:24:04 +0900

    feat: update page text

--- src/pageText.js
+++ src/pageText.js
@@ -14,19 +14,19 @@ export const pageText = `{
         "title": "Software Engineer at Rakuten",
         "link": "https://global.rakuten.com/corp/",
         "time": "Apr 2022 - Present",
-        "desc": "Developing and improving B2B web apps for Rakuten Mobile, including a low-code app builder and a phone plan customer site.\\nReact, CSS, Angular, NestJS"
+        "desc": "Developing and improving B2B web apps for Rakuten Mobile, including a low-code app builder and a phone plan customer portal.\\nReact, TypeScript, JavaScript, CSS, HTML, Git, Angular, NestJS"
       },
       {
         "title": "Bilingual Programming & Robotics Instructor at ROBBO",
         "link": "https://www.robbo.world",
         "time": "Aug 2021 - Dec 2021",
-        "desc": "Led lectures and activities teaching programming and engineering to children ages 8-13."
+        "desc": "Led lectures and activities teaching programming and engineering to children ages 8-13 in Japanese and English."
       },
       {
-        "title": "Grader at PSU Dept. of Computer Science",
-        "link": "https://www.pdx.edu/computer-science/",
+        "title": "Programming Assignment Grader at Portland State University",
+        "link": "https://www.pdx.edu/",
         "time": "Oct 2018 - Mar 2021",
-        "desc": "Scored and gave feedback on C++/Java programs for students in university Data Structures and OOP courses."
+        "desc": "Tested, scored, and gave feedback on C++ and Java programs for students in university Data Structures and OOP courses."
       }
     ]
   },
@@ -37,13 +37,13 @@ export const pageText = `{
         "title": "What's for Dinner?",
         "time": "Ongoing",
         "link": "https://github.com/jkmoore/whats-for-dinner",
-        "desc": "A tool for the home chef to manage their personal inventory, recipes, and more.\\nReact, Firebase"
+        "desc": "A tool for the home chef to manage their personal inventory, recipes, and more.\\nReact, CSS, Firebase"
       },
       {
         "title": "jkmoore.github.io",
         "link": "https://github.com/jkmoore/jkmoore.github.io",
         "time": "Oct 2023",
-        "desc": "My online portfolio.\\nReact, styled-components"
+        "desc": "My online portfolio.\\nReact, CSS"
       }
     ]
   }
